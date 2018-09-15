da5f9bbdcc838ea6e18b65ad7817fb00271ea792
Williams Estrada <we4841@gmail.com>
2018-09-14 19:09:26 -0500

    Implemented logic to mark a given position

--- script.rb
+++ script.rb
@@ -1,9 +1,27 @@
+#TODO: Make a metod to fill this array with these defaul values
 $marks = [["1","2","3"],["4","5","6"],["7","8","9"]]
 
 def main_menu
     print_board $marks
     print "\nSelect the number where you want to put your mark: "
     choice = gets.chomp
+    position = find_choice choice
+    if position
+        $marks[position[:row]][position[:column]] = "X" 
+        print_board $marks
+    else
+        puts "Incorrect choice"
+    end
+end
+
+def find_choice(choice)
+    $marks.each_index do |row_index|
+        if $marks[row_index].include?(choice) 
+            index = $marks[row_index].index(choice) 
+            return {row:row_index, column:index}
+        end
+    end
+    false
 end
 
 
