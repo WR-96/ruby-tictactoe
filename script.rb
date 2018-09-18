28331ae7cfe7adbc7f2caf8f0599f35bb45a167f
Williams Estrada <we4841@gmail.com>
2018-09-18 17:53:04 -0500

    Winning conditions set implemented method to check the winning conditions implemented method to transpose the matrix board

--- script.rb
+++ script.rb
@@ -1,17 +1,107 @@
-#TODO: Make a metod to fill this array with these defaul values
-$marks = [["1","2","3"],["4","5","6"],["7","8","9"]]
+$marks = [[],[],[]]
 
 def main_menu
+    player_mark = "X"
+    $marks = fill_marks 
+    choice = ""
     print_board $marks
-    print "\nSelect the number where you want to put your mark: "
-    choice = gets.chomp
-    position = find_choice choice
-    if position
-        $marks[position[:row]][position[:column]] = "X" 
+    loop do
+        print "\nSelect the number where you want to put your mark: "
+        choice = gets.chomp
+        position = find_choice choice
+        if position && player_mark == "X"
+            $marks[position[:row]][position[:column]] = "X" 
+            player_mark = "O"
+        elsif position && player_mark == "O"
+            $marks[position[:row]][position[:column]] = "O" 
+            player_mark = "X"
+        else
+            puts "Incorrect choice"
+        end
         print_board $marks
-    else
-        puts "Incorrect choice"
+
+        if check_rules $marks
+            puts "Another game? y/n" 
+            choice = gets.chomp.downcase
+            if choice == "y"
+                $marks = fill_marks
+                print_board $marks
+            else
+                puts "Adios"
+            end
+        end
+        break if choice == "n"
+
     end
+end
+
+def check_rules(board)
+    #finish_game = false
+    def linear_win(matrix)
+        matrix.each do |row|
+            winner = row.uniq
+            if winner.length == 1
+                puts "Winner is " << winner[0] 
+                return true
+            end
+        end
+        false
+    end
+
+    def diagonal_win(matrix)
+        diagonal_LR = []
+        diagonal_RL = []
+
+        index_LR = 0
+        index_RL = -1
+
+        matrix.each do |row|
+            diagonal_LR << row[index_LR]
+            diagonal_RL << row[index_RL]
+            index_LR += 1
+            index_RL -= 1
+            next
+        end
+
+        diagonal_LR.uniq!
+        diagonal_RL.uniq!
+
+        if diagonal_LR.length == 1
+            puts "Winner is " << diagonal_LR[0]
+            return true
+        elsif diagonal_RL.length == 1
+            puts "Winner is " << diagonal_RL[0]
+            return true
+        end
+        false
+    end
+
+    linear_win(board) || linear_win(transpose board) || diagonal_win(board)
+end
+
+def transpose(matrix)
+    new_matrix = []
+    matrix.each_index do |row|
+        new_matrix[row] = []
+        matrix[row].each_index do |column| 
+           new_matrix[row].push(matrix[column][row])
+        end
+    end
+    new_matrix 
+end
+
+
+def fill_marks
+    matrix = []
+    count = 0
+    for i in 0...3 do
+        matrix[i] = []
+        for j in 1..3 do
+            count += 1
+            matrix[i].push("#{count}")
+        end
+    end
+    matrix
 end
 
 def find_choice(choice)
@@ -23,7 +113,6 @@ def find_choice(choice)
     end
     false
 end
-
 
 def print_board(marks)
     puts "   |   |   "
